ff1fbd0bf3c66452340bf4c9d79830f1a4afe827
RSakultarawattn <ray.sakultarawattn@gmail.com>
2021-02-17 13:59:44 -0800

    get posts selector created and test passing

---
+++ src/selectors/postSelectors.test.js
@@ -0,0 +1,15 @@
+import { countPosts, getPosts } from './postSelectors';
+
+describe('post selectors', () => {
+  it('select the list of posts from state', () => {
+    const state = {
+      posts: [{ title: 'life and stuff', 
+        body: 'things and stuff about life and stuff' }]
+    };
+
+    const posts = getPosts(state);
+
+    expect(posts).toEqual([{ title: 'life and stuff', 
+      body: 'things and stuff about life and stuff' }]);
+  });
+});
