1e168ac138194f96dc383cc1901ca8577e428734
RSakultarawattn <ray.sakultarawattn@gmail.com>
2021-02-18 13:48:51 -0800

    create comment test passes

--- src/actions/commentActions.js
+++ src/actions/commentActions.js
@@ -1,11 +1,11 @@
 export const CREATE_COMMENT = 'CREATE_COMMENT';
-export const createComment = comment => ({
+export const createComment = (index) => ({
   type: CREATE_COMMENT, 
-  payload: comment
+  payload: { index }
 });
 
 export const DELETE_COMMENT = 'DELETE_COMMENT';
-export const deleteComment = (title) => ({
+export const deleteComment = (index, comment) => ({
   type: DELETE_COMMENT,
-  payload: title
+  payload: { index, comment }
 });
